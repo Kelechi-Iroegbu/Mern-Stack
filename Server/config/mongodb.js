613a80e476d0003df10ba2c191d26a66ceb48f92
kelly <kiroegbu@gmail.com>
2025-05-01 20:29:16 +0100

    added frontend url to cors allowed list

--- Server/config/mongodb.js
+++ Server/config/mongodb.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 
 const connectDB = async ()=>{
-    mongoose.connection.on('Connected');
+    try{
+        // mongoose.connection.on('Connected');
     await mongoose.connect(`${process.env.MONGODB_URI}/MERN-AUTH`);
+        console.log("database connected")
+    }catch (error){
+        console.log(error)
 
-                                                                             
+    }
+                                                                   
 }
 
 export default connectDB;
